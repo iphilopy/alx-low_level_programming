ef1e15cd4dee0f9946ac4addf35bd18c488642fa
Absulrahman Sani <philosopeace@yahoo.com>
2022-06-07 14:11:08 -0700

    task 14

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+
+/**
+ * natural - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: void
+ */
+
+
+
+int main(void)
+
+{
+
+		int i;
+
+			float a, b, r;
+
+				a = 1;
+
+					b = 2;
+
+						r= 0;
+
+
+
+							printf("1, 2");
+
+								for(i = 1; i <= 96; i++)
+
+										{
+
+													r = a + b;
+
+															a = b;
+
+																	b = r;
+
+																			printf(", %.0f",r);
+
+																				}
+
+									putchar('\n');
+
+										return (0);
+    
+
+}
