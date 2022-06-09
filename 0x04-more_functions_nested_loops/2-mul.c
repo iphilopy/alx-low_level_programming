c5a5543f098ab14833291a594a17d3f0bcd5bf4e
Absulrahman Sani <philosopeace@yahoo.com>
2022-06-09 10:03:23 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,25 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * mul -multiplies two integers
- * @a: print int
- * @b: print int
+ * mul -  checks for checks for a digit (0 through 9).
+ * @a: a -  Variable
+ * @b: b - variable
  *
  * Return: Always 0.
  */
-
-
 
 int mul(int a, int b)
 
 {
 
-	return (a * b);
+		int mul;
+
+
+
+			mul = a * b;
+
+
+
+				return (mul);
 
 }
