298fb3d3d120e86d924bb02cff5b9da1ed8afd88
Absulrahman Sani <philosopeace@yahoo.com>
2022-06-24 11:15:46 -0700

    0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -13,20 +13,20 @@ cat > main.c
 #include "main.h"
 
 /**
-
- * main - Entry point
-
- * Return: 0.
-
- */
+ *
+ *  * main - Entry point
+ *
+ *   * Return: 0.
+ *
+ *    */
 
 int main(void)
 
 {
 
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 
-return (0);
+	return (0);
 
 }
 
